68dffc72ee3372b6cdceabfced28e54bce6e6003
Gregor Sindl <gregor4005@gmail.com>
2019-08-20 21:29:24 +0200

    Split PushCrewClient

---
+++ src/GregsStack.PushCrew.Net.Api/PushCrewClient.Segments.cs
@@ -0,0 +1,60 @@
+﻿namespace GregsStack.PushCrew.Net.Api
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using Newtonsoft.Json;
+
+    using Request;
+
+    using Response;
+
+    public partial class PushCrewClient
+    {
+        private const string SegmentsRelativeUri = "segments";
+
+        public async Task<SegmentResponse> AddSegmentAsync(string name)
+        {
+            var dict = new Dictionary<string, string> { { "name", name } };
+            return await this.PostAsync<Dictionary<string, string>, SegmentResponse>(dict, SegmentsRelativeUri);
+        }
+
+        public async Task<SegmentsResponse> ListSegmentsAsync()
+        {
+            return await this.GetAsync<SegmentsResponse>(SegmentsRelativeUri);
+        }
+
+        public async Task<SegmentResponse> AddSubscribersToSegmentAsync(long segmentId, ICollection<string> subscriberList)
+        {
+            var relativeUri = $"{SegmentsRelativeUri}/{segmentId}/subscribers";
+
+            var validSubscriberList = subscriberList ?? throw new ArgumentNullException(nameof(subscriberList));
+            var subscribers = new Dictionary<string, ICollection<string>> { { "subscriber_list", validSubscriberList } };
+            var subscriberRequest = new SendMessageSubscribersRequest
+            {
+                SubscriberList = JsonConvert.SerializeObject(subscribers)
+            };
+
+            return await this.PostAsync<SendMessageSubscribersRequest, SegmentResponse>(subscriberRequest, relativeUri);
+        }
+
+        public async Task<SubscribersResponse> ListSubscribersInSegmentAsync(long segmentId, int page = 1, int perPage = 1024)
+        {
+            var relativeUri = $"{SegmentsRelativeUri}/{segmentId}/subscribers?page={page}&per_page={perPage}";
+            return await this.GetAsync<SubscribersResponse>(relativeUri);
+        }
+
+        public async Task<StatusResponse> RemoveSubscribersAsync(long segmentId, RemoveSubscriberRequest removeSubscriberRequest)
+        {
+            var relativeUri = $"{SegmentsRelativeUri}/{segmentId}/subscribers";
+            return await this.PutAsync<RemoveSubscriberRequest, StatusResponse>(removeSubscriberRequest, relativeUri);
+        }
+
+        public async Task<StatusResponse> DeleteSegmentAsync(long segmentId)
+        {
+            var relativeUri = $"{SegmentsRelativeUri}/{segmentId}";
+            return await this.DeleteAsync<StatusResponse>(relativeUri);
+        }
+    }
+}
--- src/GregsStack.PushCrew.Net.Api/PushCrewClient.cs
+++ src/GregsStack.PushCrew.Net.Api/PushCrewClient.cs
@@ -1,7 +1,6 @@
 ﻿namespace GregsStack.PushCrew.Net.Api
 {
     using System;
-    using System.Collections.Generic;
     using System.Net;
     using System.Net.Http;
     using System.Net.Http.Formatting;
@@ -11,15 +10,10 @@
 
     using Exceptions;
 
-    using Newtonsoft.Json;
     using Newtonsoft.Json.Converters;
     using Newtonsoft.Json.Serialization;
 
-    using Request;
-
-    using Response;
-
-    public class PushCrewClient : IPushCrewClient
+    public partial class PushCrewClient : IPushCrewClient
     {
         private readonly IHttpClientFactory clientFactory;
         private readonly JsonMediaTypeFormatter jsonFormatter;
@@ -39,112 +33,6 @@
 
             this.jsonFormatter.SerializerSettings.Converters.Add(new StringEnumConverter());
             this.jsonFormatter.SerializerSettings.Converters.Add(new BooleanConverter());
-        }
-
-        public async Task<SendMessageResponse> SendAllSubscribersAsync(SendMessageRequest request)
-        {
-            const string relativeUri = "send/all";
-            return await this.PostAsync<SendMessageRequest, SendMessageResponse>(request, relativeUri);
-        }
-
-        public async Task<SendMessageResponse> SendSubscribersInSegmentAsync(SendMessageRequest request, long segmentId)
-        {
-            var relativeUri = $"send/segment/{segmentId}";
-            return await this.PostAsync<SendMessageRequest, SendMessageResponse>(request, relativeUri);
-        }
-
-        public async Task<SendMessageResponse> SendSubscribersAsync(SendMessageRequest request, ICollection<string> subscriberList)
-        {
-            const string relativeUri = "send/list";
-
-            var validRequest = request ?? throw new ArgumentNullException(nameof(request));
-            var validSubscriberList = subscriberList ?? throw new ArgumentNullException(nameof(subscriberList));
-            var subscribers = new Dictionary<string, ICollection<string>> { { "subscriber_list", validSubscriberList } };
-            var subscriberRequest = (SendMessageSubscribersRequest)validRequest;
-            subscriberRequest.SubscriberList = JsonConvert.SerializeObject(subscribers);
-
-            return await this.PostAsync<SendMessageSubscribersRequest, SendMessageResponse>(subscriberRequest, relativeUri);
-        }
-
-        public async Task<SendMessageResponse> SendSubscriberAsync(SendMessageRequest request, string subscriberId)
-        {
-            const string relativeUri = "send/individual";
-
-            var validRequest = request ?? throw new ArgumentNullException(nameof(request));
-            var validSubscriberId = subscriberId ?? throw new ArgumentNullException(nameof(subscriberId));
-            var subscriberRequest = (SendMessageSubscriberRequest)validRequest;
-            subscriberRequest.SubscriberId = validSubscriberId;
-
-            return await this.PostAsync<SendMessageSubscriberRequest, SendMessageResponse>(subscriberRequest, relativeUri);
-        }
-
-        public async Task<NotificationStatusResponse> CheckNotificationRequestStatusAsync(string id)
-        {
-            var relativeUri = $"checkstatus/{id}";
-            return await this.GetAsync<NotificationStatusResponse>(relativeUri);
-        }
-
-        public async Task<ScheduleMessageResponse> ScheduleAllSubscribersAsync(ScheduleMessageRequest request)
-        {
-            const string relativeUri = "send/all";
-            return await this.PostAsync<ScheduleMessageRequest, ScheduleMessageResponse>(request, relativeUri);
-        }
-
-        public async Task<ScheduleMessageResponse> ScheduleSegmentAsync(ScheduleMessageRequest request, long segmentId)
-        {
-            var relativeUri = $"send/segment/{segmentId}";
-            return await this.PostAsync<ScheduleMessageRequest, ScheduleMessageResponse>(request, relativeUri);
-        }
-
-        public async Task<SegmentResponse> AddSegmentAsync(string name)
-        {
-            const string relativeUri = "segments";
-            var dict = new Dictionary<string, string> { { "name", name } };
-            return await this.PostAsync<Dictionary<string, string>, SegmentResponse>(dict, relativeUri);
-        }
-
-        public async Task<SegmentsResponse> ListSegmentsAsync()
-        {
-            const string relativeUri = "segments";
-            return await this.GetAsync<SegmentsResponse>(relativeUri);
-        }
-
-        public async Task<SegmentResponse> AddSubscribersToSegmentAsync(long segmentId, ICollection<string> subscriberList)
-        {
-            var relativeUri = $"segments/{segmentId}/subscribers";
-
-            var validSubscriberList = subscriberList ?? throw new ArgumentNullException(nameof(subscriberList));
-            var subscribers = new Dictionary<string, ICollection<string>> { { "subscriber_list", validSubscriberList } };
-            var subscriberRequest = new SendMessageSubscribersRequest
-            {
-                SubscriberList = JsonConvert.SerializeObject(subscribers)
-            };
-
-            return await this.PostAsync<SendMessageSubscribersRequest, SegmentResponse>(subscriberRequest, relativeUri);
-        }
-
-        public async Task<SubscribersResponse> ListSubscribersInSegmentAsync(long segmentId, int page = 1, int perPage = 1024)
-        {
-            var relativeUri = $"segments/{segmentId}/subscribers?page={page}&per_page={perPage}";
-            return await this.GetAsync<SubscribersResponse>(relativeUri);
-        }
-
-        public async Task<SubscribersResponse> ListSegmentsOfSubscriberAsync(string subscriberId)
-        {
-            var relativeUri = $"subscribers/{subscriberId}/segments";
-            return await this.GetAsync<SubscribersResponse>(relativeUri);
-        }
-
-        public async Task<StatusResponse> RemoveSubscribersAsync(long segmentId, RemoveSubscriberRequest removeSubscriberRequest)
-        {
-            var relativeUri = $"segments/{segmentId}/subscribers";
-            return await this.PutAsync<RemoveSubscriberRequest, StatusResponse>(removeSubscriberRequest, relativeUri);
-        }
-
-        public async Task<StatusResponse> DeleteSegmentAsync(long segmentId)
-        {
-            var relativeUri = $"segments/{segmentId}";
-            return await this.DeleteAsync<StatusResponse>(relativeUri);
         }
 
         private HttpClient CreateClient()
