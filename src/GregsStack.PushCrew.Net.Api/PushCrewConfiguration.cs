0ed62c1f442ee4ba4d85ec7f84cbaaebbb8be5c9
Gregor Sindl <gregor4005@gmail.com>
2019-08-20 20:48:09 +0200

    Use props instead of fields

--- src/GregsStack.PushCrew.Net.Api/PushCrewConfiguration.cs
+++ src/GregsStack.PushCrew.Net.Api/PushCrewConfiguration.cs
@@ -4,8 +4,8 @@
 
     public static class PushCrewConfiguration
     {
-        public static readonly string ClientName = nameof(PushCrewClient);
+        public static string ClientName { get; } = nameof(PushCrewClient);
 
-        public static readonly Uri BaseUri = new Uri("https://pushcrew.com/api/v1/");
+        public static Uri BaseUri { get; } = new Uri("https://pushcrew.com/api/v1/");
     }
 }
