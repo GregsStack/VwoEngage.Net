31356504ea3782700a609ab0d974a7c2e4403cd4
Gregor Sindl <gregor4005@gmail.com>
2019-09-03 22:57:44 +0200

    Add description

--- src/GregsStack.VwoEngage.Net.Api/VwoEngageConfiguration.cs
+++ src/GregsStack.VwoEngage.Net.Api/VwoEngageConfiguration.cs
@@ -4,8 +4,14 @@
 
     public static class VwoEngageConfiguration
     {
+        /// <summary>
+        /// Default client name to resolve named <see cref="System.Net.Http.HttpClient"/> instance.
+        /// </summary>
         public static string ClientName { get; } = nameof(VwoEngageClient);
 
+        /// <summary>
+        /// Base VWO Engage API URL.
+        /// </summary>
         public static Uri BaseUri { get; } = new Uri("https://pushcrew.com/api/v1/");
     }
 }
